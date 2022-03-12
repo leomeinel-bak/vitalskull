1c53bcada7e400ba0cf1868c08eefecc0a911872
TamrielNetwork <contact@tamriel.me>
2022-03-12 18:01:02 +0100

    Fix codeStyle

--- src/main/java/com/tamrielnetwork/vitalskull/utils/commands/CmdSpec.java
+++ src/main/java/com/tamrielnetwork/vitalskull/utils/commands/CmdSpec.java
@@ -86,9 +86,11 @@ public class CmdSpec {
 	private static boolean isOnCooldown(@NotNull CommandSender sender) {
 		Player senderPlayer = (Player) sender;
 		boolean isOnCooldown = main.getConfig()
-		                           .getBoolean("cooldown.enabled") && !sender.hasPermission("vitalskull.cooldown.bypass") && cooldownMap.containsKey(senderPlayer.getUniqueId());
+		                           .getBoolean("cooldown.enabled") && !sender.hasPermission(
+				"vitalskull.cooldown.bypass") && cooldownMap.containsKey(senderPlayer.getUniqueId());
 		if (isOnCooldown) {
-			String timeRemaining = String.valueOf(cooldownMap.get(senderPlayer.getUniqueId()) - System.currentTimeMillis() / 1000);
+			String timeRemaining = String.valueOf(
+					cooldownMap.get(senderPlayer.getUniqueId()) - System.currentTimeMillis() / 1000);
 			Chat.sendMessage(sender, Map.of("%time-left%", timeRemaining), "cooldown-active");
 			return true;
 		}
